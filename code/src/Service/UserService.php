04e09c1980e0893d24dbfe02796f1e303e895f91
patrickmaynard <patrickmaynard@users.noreply.github.com>
2021-11-21 11:03:27 +0100

    Increase php docker image memory limit, tweak user admin check

--- code/src/Service/UserService.php
+++ code/src/Service/UserService.php
@@ -6,6 +6,7 @@ use App\Entity\SonataUserUser;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\UserBundle\Model\GroupInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Yaml\Parser;
 
 class UserService implements UserServiceInterface
 {
@@ -17,30 +18,55 @@ class UserService implements UserServiceInterface
      */
     private $entityManager;
 
+    private $projectDir;
+
+    private $roleHierarchy;
+
     public function __construct(
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        string $projectDir
     ) {
         $this->entityManager = $entityManager;
+        $this->projectDir = $projectDir;
+        $this->roleHierarchy = $this->initializeRoleHierarchy();
     }
 
     /**
-     * Type hinting for the TokenStorageInterface input is off for now.
-     * It will stay off until I find a way to mock that interface in tests.
-     * (Currently, it causes a MethodCannotBeConfiguredException.)
-     *
-     * @param TokenStorageInterface $user
+     * @param TokenInterface $token
      * @return bool
      */
     public function checkApiUserIsAdmin(TokenInterface $token) : bool
     {
+        /** @var SonataUserUser $user */
         $user = $token->getUser();
-        if (
-            !$user->hasRole('ROLE_ADMIN')  &&
-            !$user->hasRole('ROLE_SUPER_ADMIN')
-        ) {
-            return false;
+        $allRolesFlat = $this->getAllRolesFlat($user);
+        if (in_array('ROLE_ADMIN', $allRolesFlat)) {
+            return true;
         }
-        return true;
+        return false;
+    }
+
+    /**
+     * @param string $roleName
+     * @return array
+     */
+    public function getAllSubRolesRecursively(string $roleName) : array
+    {
+        // We might already be at the bottom of the hierarchy.
+        // If so, just return this name in an array.
+        if (!array_key_exists($roleName, $this->roleHierarchy)) {
+            return [$roleName];
+        }
+
+        //But if we're not at the bottom yet, we want an array of all subroles.
+        $roles = [];
+        $roles[] = $roleName;
+        foreach ($this->roleHierarchy[$roleName] as $key => $value) {
+            $roles = array_merge(
+                $roles, $this->getAllSubRolesRecursively($value)
+            );
+        }
+        return $roles;
     }
 
     /**
@@ -59,5 +85,42 @@ class UserService implements UserServiceInterface
             ->where($qb->expr()->eq('g.id', $group->getId()));
 
         return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @return mixed
+     */
+    private function initializeRoleHierarchy()
+    {
+        $yaml = new Parser();
+        $arrayOfOptions = $yaml->parse(
+            file_get_contents(
+                $this->projectDir.'/config/packages/security.yaml'
+            )
+        );
+        $roleHierarchy = $arrayOfOptions['security']['role_hierarchy'];
+
+        return $roleHierarchy;
+    }
+
+    /**
+     * @param SonataUserUser $user
+     * @return array
+     */
+    private function getAllRolesFlat(SonataUserUser $user): array
+    {
+        $topLevelRolesIncludingGroupRoles = $user->getRoles();
+        $allRolesFlat = [];
+        foreach ($topLevelRolesIncludingGroupRoles as $roleName) {
+            foreach (
+                $this->getAllSubRolesRecursively($roleName) as $extraRole
+            ) {
+                if (!in_array($extraRole, $allRolesFlat)) {
+                    $allRolesFlat[] = $extraRole;
+                }
+            }
+        }
+
+        return $allRolesFlat;
     }
 }
